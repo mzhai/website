4f04b9276b90cf46c1a18cb1ecb6130e2dd5a804
May Zhai <mzhai9@gmail.com>
2016-01-09 16:00:10 -0500

    built project

--- dist/assets/may.js
+++ dist/assets/may.js
@@ -793,7 +793,7 @@ catch(err) {
 if (runningTests) {
   require("may/tests/test-helper");
 } else {
-  require("may/app")["default"].create({"name":"may","version":"0.0.0+1d1c1260"});
+  require("may/app")["default"].create({"name":"may","version":"0.0.0+7d0316da"});
 }
 
 /* jshint ignore:end */
